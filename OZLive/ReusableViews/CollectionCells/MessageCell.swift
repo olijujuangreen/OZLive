bf30fd38b294676ebf721f39b05f9aa65ea439e8
Olijujuan Green <nihongojaydee@gmail.com>
2023-02-14 17:03:44 -0500

    fixed chat to update correctly

--- OZLive/ReusableViews/CollectionCells/MessageCell.swift
+++ OZLive/ReusableViews/CollectionCells/MessageCell.swift
@@ -12,17 +12,18 @@ class MessageCell: UITableViewCell {
     
     let usernameLabel: UILabel = {
         let label = UILabel()
-        label.text = "username"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let messageLabel: UILabel = {
         let label = UILabel()
-        label.text = "contents of the message"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -33,18 +34,21 @@ class MessageCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    
+    
     func setupViews() {
         [usernameLabel, messageLabel].forEach { addSubview($0) }
         
         NSLayoutConstraint.activate([
         
-            usernameLabel.topAnchor.constraint(equalTo: topAnchor, constant: Constraints.standardVerticalSpacing.rawValue),
+            usernameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
             usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constraints.widePadding.rawValue),
             usernameLabel.widthAnchor.constraint(equalToConstant: Constraints.usernameLabelWidth.rawValue),
             
             messageLabel.leadingAnchor.constraint(equalTo: usernameLabel.trailingAnchor, constant: Constraints.widePadding.rawValue),
             messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-            messageLabel.topAnchor.constraint(equalTo: topAnchor)
+            messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
         
         ])
     }
